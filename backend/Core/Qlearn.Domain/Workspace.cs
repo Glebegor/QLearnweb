0056ebce4a1b871225e280f5b775f3e273a52bd9
Glebegor <glebegor2220@gmail.com>
2024-03-07 10:30:25 +0100

    BE: Changed workspace.

--- backend/Core/Qlearn.Domain/Workspace.cs
+++ backend/Core/Qlearn.Domain/Workspace.cs
@@ -2,5 +2,8 @@ namespace Qlearn.Domain;
 
 public class Workspace
 {
-    
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
 }
