642478d2a3caf4452df4c0c4ce074fc110b45580
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-05-21 23:24:26 +0200

    Created Http Handler.

--- backend/Bootstrap/Server.cs
+++ backend/Bootstrap/Server.cs
@@ -1,16 +1,31 @@
+using System.Net;
+using System.Net.Sockets;
+
 namespace QLearn.Bootstrap;
 
 public class Server
 {
+    public HttpListener httpListener;
     private int port;
     private string host;
     private string url;
     
-    public Server()
+    public Server(string host, int port)
     {
-        this.args = args;
-        this.port = 5000;
-        this.host = "http://localhost";
+        this.httpListener = new HttpListener();
+        this.port = port;
+        this.host = host;
         this.url = $"{this.host}:{this.port}";
+        
+        httpListener.Prefixes.Add($"http://{this.url}/");
+    }
+    
+    public static async Task Run()
+    {
+        Console.WriteLine($"Server started at {this.url}");
+        while (true)
+        {
+            
+        }
     }
 }
