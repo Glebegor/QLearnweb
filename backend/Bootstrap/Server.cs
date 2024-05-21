f8c6f8de15b4249bcf5124722cf8971a0a89132d
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-05-21 23:21:06 +0200

    Created structure of server.

--- backend/Bootstrap/Server.cs
+++ backend/Bootstrap/Server.cs
@@ -2,5 +2,15 @@ namespace QLearn.Bootstrap;
 
 public class Server
 {
+    private int port;
+    private string host;
+    private string url;
     
+    public Server()
+    {
+        this.args = args;
+        this.port = 5000;
+        this.host = "http://localhost";
+        this.url = $"{this.host}:{this.port}";
+    }
 }
