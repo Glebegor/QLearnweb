421b933d748fb295ef0fb5bd4e083bb20b9355d3
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-05-23 17:19:05 +0200

    Need to work with config.

--- backend/Bootstrap/App.cs
+++ backend/Bootstrap/App.cs
@@ -1,13 +1,28 @@
+using System;
+using System.IO;
+using System.Text.Json;
+
 namespace backend.Bootstrap;
 
 
 public class App
 {
     private Server server;
-
+    private Config config;
+    
     public App()
     {
+        // Config init
+        this.config = GetConfig("../Config/config.json");
+        // Server init
         this.server = new Server("localhost", 3500);
+    }
+
+    public Config GetConfig(string path)
+    {
+        string jsonString = File.ReadAllText(path);
+        Config config = JsonSerializer.Deserialize<Config>(jsonString);
+        return config;
     }
     public void Run()
     {
--- backend/Bootstrap/Config.cs
+++ backend/Bootstrap/Config.cs
@@ -1,7 +1,23 @@
 namespace backend.Bootstrap;
 
 
+public class ServerConfig
+{
+    public string HOST { get; set; }
+    public int PORT { get; set; }
+}
+
+public class DatabaseConfig
+{
+    public string HOST { get; set; }
+    public int PORT { get; set; }
+    public string NAME { get; set; }
+    public string USER { get; set; }
+}
+
 public class Config
 {
-    
+    public ServerConfig Server { get; set; }
+    public DatabaseConfig Database { get; set; }
 }
+
