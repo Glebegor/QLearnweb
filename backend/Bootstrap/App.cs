cf417e073ea48e0a40ddd4ab0fd0d8fd6271aaff
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-05-23 16:54:58 +0200

    Creating handlers for server.

--- backend/Bootstrap/App.cs
+++ backend/Bootstrap/App.cs
@@ -3,5 +3,14 @@ namespace backend.Bootstrap;
 
 public class App
 {
-    
+    private Server server;
+
+    public App()
+    {
+        this.server = new Server("localhost", 3500);
+    }
+    public void Run()
+    {
+        server.Run();
+    }
 }
--- backend/Bootstrap/Server.cs
+++ backend/Bootstrap/Server.cs
@@ -5,28 +5,28 @@ namespace backend.Bootstrap;
 
 public class Server
 {
-    public HttpListener httpListener;
+    public HttpListener HttpListener;
     private int port;
     private string host;
     private string url;
     
     public Server(string host, int port)
     {
-        this.httpListener = new HttpListener();
+        this.HttpListener = new HttpListener();
         this.port = port;
         this.host = host;
         this.url = $"{this.host}:{this.port}";
         
-        httpListener.Prefixes.Add($"http://{this.url}/");
+        HttpListener.Prefixes.Add($"http://{this.url}/");
     }
     
     public async Task Run()
     {
         Console.WriteLine($"Server started at {this.url}");
         Console.WriteLine("________________________________________________________");
-        while (true)
-        {
-            
-        }
+        // while (true)
+        // {
+        //     
+        // }
     }
 }
