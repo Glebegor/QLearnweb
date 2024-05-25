d6773d772c9876ac1459adf9ea3150eb98316959
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-05-25 23:55:32 +0200

    setuped user entity.

--- backend/Core/Common/Database/User.cs
+++ backend/Core/Common/Database/User.cs
@@ -3,5 +3,8 @@ namespace backend.Core.Common.Database;
 
 public class User
 {
-    
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
 }
