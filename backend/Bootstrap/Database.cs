2c93a118a10d9d9d6881356b5676c255e478d1fa
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-05-29 22:27:05 +0200

    Created migrations by python.

--- backend/Bootstrap/Database.cs
+++ backend/Bootstrap/Database.cs
@@ -5,26 +5,16 @@ namespace backend.Bootstrap
     public class Database
     {
         private readonly Config config;
+        public NpgsqlConnection Connection;
 
         public Database(Config config)
         {
             config = config;
-        }
+            
+            string connectionString = $"Host={config.Database.HOST};Port={config.Database.PORT};Database={config.Database.NAME};Username={config.Database.USER};Password={config.Database.PASSWORD}"; 
+            NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+            this.Connection = connection;
 
-        public void ConfigureDatabase()
-        {
-            string connectionString = $"Host={config.Database.HOST};Port={config.Database.PORT};Database={config.Database.NAME};Username={config.Database.USER};Password={config.Database.PASSWORD}";
-            using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
-            connection.Open();
-
-            using NpgsqlCommand ping = new NpgsqlCommand("SELECT * FROM users;", connection);
-
-            using NpgsqlDataReader reader = ping.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Console.WriteLine(reader["column_name"]);
-            }
         }
     }
 }
