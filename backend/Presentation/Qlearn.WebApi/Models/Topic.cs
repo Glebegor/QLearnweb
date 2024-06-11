8f5220aac39c0b68ea1b6482e4d07fd5ca47b0ef
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-11 22:28:32 +0200

    Need to add classlib.

---
+++ backend/Presentation/Qlearn.WebApi/Models/Topic.cs
@@ -0,0 +1,10 @@
+namespace Qlean.WebApi.Models
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        
+    }
+}
---
+++ backend/Presentation/Qlearn.WebApi/Models/User.cs
@@ -0,0 +1,13 @@
+
+
+namespace Qlean.WebApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+    }
+}
