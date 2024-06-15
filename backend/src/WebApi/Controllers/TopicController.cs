4a684e9dcef3e4b1bc6a8a6530056e3c5cb5ee90
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-15 20:27:43 +0200

    BE: Topic created init controller.

--- backend/src/WebApi/Controllers/TopicController.cs
+++ backend/src/WebApi/Controllers/TopicController.cs
@@ -1,7 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace WebApi.Controllers
 {
-    public class TopicController
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class TopicController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World");
+        }
         
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("Hello World");
+        }
+        
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Hello World");
+        }
+        
+        [HttpPut("{id}")]
+        public IActionResult Put(int id)
+        {
+            return Ok("Hello World");
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok("Hello World");
+        }
     }
 };
