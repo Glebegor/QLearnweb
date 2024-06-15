47af6a3400ae6c6a8202a22a458a5aaaa389038d
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-15 20:11:47 +0200

    Added swagger.

--- backend/src/WebApi/Startup.cs
+++ backend/src/WebApi/Startup.cs
@@ -1,4 +1,6 @@
-
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace WebApi
 {
@@ -13,6 +15,11 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -21,6 +28,18 @@ namespace WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name V1");
+                // Optionally, specify the Swagger UI version
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API Name V1");
+            });
 
             app.UseRouting();
 
